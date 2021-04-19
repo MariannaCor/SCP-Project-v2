3ec5143ad57901bc983ce00f414917d2de5481bf
Salvo <sfiorilla01@gmail.com>
2021-04-19 13:05:04 +0200

    update Sorting.scala

--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -1,51 +1,60 @@
-class Sorting {
+import scala.util.Random
+
+object Sorting {
 
   def main(args: Array[String]): Unit = {
-    var myarr = new Array[SimpleTuple]( 3 )
-    myarr = Array ( SimpleTuple ( 1, 3.45 ), SimpleTuple ( 2, 4.65 ), SimpleTuple ( 3, 7.68 ) )
+    var myarr = new Array[SimpleTuple]( 15 )
+    //myarr = A// SimpleTuple ( 1, 3.45 ), SimpleTuple ( 2, 4.65 ), SimpleTuple ( 3, 7.68 ) )
+    for (i <- (0 until 15)){
+      val x = Math.abs(new Random().nextInt(1000)/10 )
+      myarr(i) = SimpleTuple(i, x)
+    }
 
-    parMergeSort(myarr, 3);
+    myarr.map( x=> println(x.idOfTheDoc + " , "+x.value) )
+    parMergeSort(myarr, 5);
   }
 
+
+  def merge(src: Array[SimpleTuple], dst: Array[SimpleTuple], from: Int, mid: Int, until: Int): Unit = {
+    quickS((src :+ dst),from, until )
+  }
 
   def parMergeSort(xs: Array[SimpleTuple], maxDepth: Int): Unit = {
     val ys = new Array[SimpleTuple](xs.length)
 
     def sort(from: Int, until: Int, depth: Int): Unit = {
+
       if (depth == maxDepth) {
         quickS(xs, from, until)
       } else {
         val mid = (from + until) / 2
-        parallel(sort(mid, until, depth + 1),
+        //parallel
+          (sort(mid, until, depth + 1),
           sort(from, mid, depth + 1))
+
         val flip = (maxDepth - depth) % 2 == 0
         val src = if (flip) ys else xs
         val dst = if (flip) xs else ys
-        merge(src, dst, from, mid, until)
+
+        merge(src, dst,from, mid, until)
       }
     }
 
+
+    sort(0,xs.length,maxDepth +1 )
+
+
   }
 
-  private def merge(xs: List[SimpleTuple], ys: List[SimpleTuple]): Array[SimpleTuple] =
-  xs match {
-    case Nil => ys
-    case x :: xs1 =>
-      ys match {
-        case Nil => xs
-        case y :: ys1 =>
-          if( x.value > y.value ) x :: merge ( xs1, ys )
-          else y :: merge ( xs, ys1 )
-      }
-  }
+
 
   private def quickS(xs: Array[SimpleTuple], from: Int, until: Int) = {
 
     // do the sorting
-    val sortedArray = quickSort(xs)
+   /*a val sortedArray = quickSort(xs)
 
     // print the ordered array
-    sortedArray.foreach(println)
+    sortedArray.foreach(println)*/
 
     // the quicksort recursive algorithm
     def quickSort(xs: Array[SimpleTuple]): Array[SimpleTuple] = {
@@ -53,9 +62,9 @@ class Sorting {
       else {
         val pivot = xs(xs.length / 2)
         Array.concat(
-          quickSort(xs filter (pivot > _.value)),
-          xs filter (pivot == _.value),
-          quickSort(xs filter (pivot < _.value)))
+          quickSort(xs filter (pivot.value > _.value)),
+          xs filter (pivot.value == _.value),
+          quickSort(xs filter (pivot.value < _.value)))
       }
     }
   }
