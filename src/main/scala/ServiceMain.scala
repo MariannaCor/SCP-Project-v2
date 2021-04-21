45f7288c40a08c809e50f3ba822ea46e697371ba
Salvo <sfiorilla01@gmail.com>
2021-04-21 18:11:00 +0200

    update

--- src/main/scala/ServiceMain.scala
+++ src/main/scala/ServiceMain.scala
@@ -1,17 +1,16 @@
 import org.apache.spark.sql.functions.{col, explode}
-
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 
-
 import scala.collection.mutable
+import scala.collection.parallel.mutable.ParArray
 
 //DONE: pre processing db con flag.
 //TODO: persist su parOkapi. (S&M)
 //TODO: sync su parOkapi. (S&M)
 //TODO: data pre processing versione RDD.(M)
-//TODO: rinominare il file. (S)
-//TODO: 1. sorting decrescente e take n 2. output finale con titolo e testo dei primi n file trovati. (S)
+//TODO: rinominare il file programmaticamente. (S) ...
+//TODO: 1. sorting decrescente e take n 2.(DONE) output finale con titolo e testo dei primi n file trovati. (S) (DONE)
 //DONE: lettura del DB da file s3.
 
 
@@ -65,28 +64,67 @@ object ServiceMain {
   *
   * */
 
+  def generateOutput(source: Array[SimpleTuple] ,sparkSession: SparkSession): Array[(Int, Double, String, String)] = {
 
-  def block_of_code(preProcessedDB: DataFrame, myQuery: Array[String], sc: SparkContext): Unit = {
+    val titles = readPreprocessedDBFromJson( "s3://scpmarysal/preProcessedDB/cleanTitle.json", sparkSession).collect().par;
+    val texts = readPreprocessedDBFromJson( " s3://scpmarysal/preProcessedDB/wikiclean.json", sparkSession).collect();
+
+    val temp: ParArray[(Int,Double,String)] = titles
+      .map { field => val id = field.getInt ( 0 ); (field, id) }
+      .map { case (field, id) => val index = source.indexOf ( id ); (field, id, index) }
+      .map { case (field, id, index) =>
+        if( index > -1 ) {
+        (id, source ( index ).value, field.getString ( 1 ))
+        }else{
+          (0, 0.0, "")
+        }
+      } //se non metto un else non torna lo stesso tipo, torna una volta la tripla una volta UNIT e quindi il primo padre fra i due è ANY
+
+   val result = for{
+     elem <- temp.filterNot( x=>  { x._1 == 0 && x._2 == 0.0 && x._3 == "" } )
+     id = elem._1;
+     index = texts.indexOf(id)
+   }yield{
+     if(index > -1) (id, elem._2, elem._3, texts(index).toString() ) else (0,0.0,"","")
+   } //non è chiaro perchè torna tipo ANY e non la tripla creata
+
+    result.filterNot(
+      x=>  { x._1 == 0 && x._2 == 0.0 && x._3 == "" }
+    ).toArray
+  }
+
+
+  def block_of_code(preProcessedDB: DataFrame, myQuery: Array[String], sc: SparkContext, sparkSession: SparkSession): Unit = {
+
     val okapi = new ParOkapiBM25(preProcessedDB,myQuery, preProcessedDB.count() )
-    val scores =  okapi.getBM25()
-    //println("ordered list of documents with score ")
-    Sorting.parMergeSort(scores,0);
-    //var path = "s3://sal1/result"
-    //parMergeSort
-    //scores reverseMap(  println(_)  )
 
-    sc.parallelize(scores).coalesce(1).saveAsTextFile("s3://scpmarysal/eccoIl30/")
+    val scores =  okapi.getBM25()
+    Sorting.parMergeSort( scores,2);
+    //var path = "s3://sal1/result"
+    var n = 50
+    lazy val firstN: Array[SimpleTuple] = scores.take(n);
+    lazy val result: Array[(Int,Double,String,String)] = generateOutput( firstN, sparkSession )
+
+    sc.parallelize(result).coalesce(1).saveAsTextFile("s3://scpmarysal/eccoIl30/")
 
   }
 
 
-  def readFullDBFromJson(path : String , spark : SparkSession) : DataFrame = {
+  def readFullDBFromJson(path : String , spark : SparkSession) : Array[DataFrame] = {
+
     val dfDB = spark.read
       .option("multiline", "true")
       .json(path)
 
     val arrDB = dfDB.selectExpr("mediawiki.page")
-    arrDB.withColumn("page", explode(col("page"))).selectExpr("page.id", "page.revision.text.__text")
+
+    var arrDF = new Array[DataFrame](2)
+
+    arrDF(0) = arrDB.withColumn("page", explode(col("page"))).selectExpr("page.id", "page.revision.text.__text")
+    arrDF(1) = arrDB.withColumn("page", explode(col("page"))).selectExpr("page.id", "page.title")
+
+    arrDF
+
   }
 
   def readPreprocessedDBFromJson(path : String , spark : SparkSession) : DataFrame = {
@@ -98,16 +136,18 @@ object ServiceMain {
 
 
   /*
-  *  -preprocess false/true keywords
+  * @args param options example
+  * -preprocess false/true keywords
   *
   * */
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf ().setAppName ( this.getClass.getName )
+    val conf = new SparkConf ().setAppName ( this.getClass.getName ).setMaster("local[*]")
     val spark: SparkSession = SparkSession.builder.config ( conf ).getOrCreate ()
     spark.sparkContext.setLogLevel ( "WARN" )
     val sc = spark.sqlContext.sparkContext
     var wikiDF : DataFrame = null
+
 
     /* id, text*/
     var wikiText : DataFrame = null
@@ -120,21 +160,28 @@ object ServiceMain {
       case "false" => {
         // READ PREPROCESSED DB
         wikiDF = readPreprocessedDBFromJson("s3://scpmarysal/preProcessedDB/wikiclean.json", spark)
-
       }
       case "true" => {
-        wikiDF = readFullDBFromJson("s3://scpmarysal/wikidb.json", spark)
+        //val path = "s3://scpmarysal/"
+        val path = "C:\\Users\\Salvo\\GitHub\\SCP-Project-v2\\"
+        val tempDF = readFullDBFromJson( path+"wikidb.json", spark)
+        wikiDF = tempDF(0);
+        val metaInfDF = tempDF(1)
 
         //PREPROCESS FULL DB
         val preProcessData = new DataPreProcessing(wikiDF)
         val preProcessedDB = preProcessData.preProcessDF()
 
+
+        metaInfDF.write.mode("overwrite").format("json").save(path+"salvo-output\\")
         //WRITE PREPROCESSED DB TO A FILE
         preProcessedDB.write
           .mode("overwrite")
           .format("json")
           .save("s3://scpmarysal/preProcessedDB")
       }
+
+      case _ => throw new IllegalArgumentException("no boolean param has been used for -preprocess command")
     }
 
     val userInput = splittedArgs._2.reduce((x,y) => x + " " + y)
@@ -145,7 +192,7 @@ object ServiceMain {
     val queryKeyword = dbQuery.select("words_clean").collect()
     val myQuery = queryKeyword(0).get(0).asInstanceOf[mutable.WrappedArray[String]].toArray[String]
 
-    time ( block_of_code(wikiDF,myQuery,sc) , spark )
+    time ( block_of_code(wikiDF,myQuery,sc,spark) , spark )
 
   }
 
--- src/main/scala/Sorting.scala
+++ src/main/scala/Sorting.scala
@@ -5,24 +5,19 @@ import utils.primitives
 
 object Sorting {
 
-  /*def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
     //var myarr = new Array[SimpleTuple]( 15 )
     //myarr = A// SimpleTuple ( 1, 3.45 ), SimpleTuple ( 2, 4.65 ), SimpleTuple ( 3, 7.68 ) )
 
     val r = scala.util.Random
-    val myarr = ( for (i <- 1 to 1000000) yield SimpleTuple( i, r.nextInt(1000000000) ) ).toArray
-    //println("array da ordinare ")
-    //myarr.take(100).map( x=> println(x))
-
-    //val res = SegmentQuickSort(myarr,0,myarr.length)
+    val myarr = ( for (i <- 1 to 1000000) yield SimpleTuple( i, r.nextInt(10000000) ) ).toArray
+    println( "L'array è ordinato ? "+checkSoundness(myarr))
     println("ordinamento di 1 MLN di istanze")
     time( blockOfCode(myarr) )
-    //SegmentQuickSort(myarr, 3, 9)
-    (myarr,2)
-    println("array ordinato ")
-    myarr.take(100).map( x=> println(x))
-
-  }*/
+    //SegmentQuickSort(myarr, 0, myarr.length)
+    println( "L'array è ordinato ? "+checkSoundness(myarr))
+    
+  }
 
   def time[R](block: => R): R = {
 
@@ -35,13 +30,24 @@ object Sorting {
     result
   }
 
+  /* it returns true if the array is ordered from higher to lower value */
+  def checkSoundness(myarr: Array[SimpleTuple]): Boolean = {
+    var i = 0 ;
+    while (i < myarr.length - 2 ){
+      if( myarr(i).value < myarr(i+1).value ) {  return false  }
+      i = i+1;
+    }
+    true
+  }
+
+
   def blockOfCode(myarr: Array[SimpleTuple]): Unit ={
-    var iter= Math.abs(Math.log(myarr.length))
-    parMergeSort(myarr,  5);
+    parMergeSort(myarr,  4);
   }
 
   def parMergeSort(xs: Array[SimpleTuple], maxDepth: Int) = {
 
+    if (maxDepth % 2 == 1) throw new Exception("max depth param must be an even number")
    //array di appoggio
    val ys = new Array[SimpleTuple](xs.length)
 
@@ -64,8 +70,8 @@ object Sorting {
    }
     sort(0,xs.length,0 )
   }
-
-  def merge(src: Array[SimpleTuple], dst: Array[SimpleTuple], from: Int, mid: Int, until: Int) = {
+/*
+*  def merge(src: Array[SimpleTuple], dst: Array[SimpleTuple], from: Int, mid: Int, until: Int) = {
 
     var left = from
     var right = mid
@@ -98,7 +104,84 @@ object Sorting {
       right += 1
     }
   }
+* */
 
+
+  def merge(src: Array[SimpleTuple], dst: Array[SimpleTuple], from: Int, mid: Int, until: Int) = {
+
+    var left = from
+    var right = mid
+    var i = from
+
+    while (left < mid && right < until) {
+      while (left < mid && src(left).value >= src(right).value) {
+        dst(i) = src(left)
+        i += 1
+        left += 1
+      }
+
+      while (right < until && src(right).value >= src(left).value) {
+        dst(i) = src(right)
+        i += 1
+        right += 1
+      }
+
+    }
+
+    while (left < mid){
+      dst(i)=src(left)
+      i += 1
+      left += 1
+    }
+
+    while (right < until) {
+      dst(i) = src(right)
+      i += 1
+      right += 1
+    }
+  }
+
+
+  def SegmentQuickSort(xs: Array[SimpleTuple], from: Int, until: Int) = {
+
+     // the quicksort recursive algorithm in the segment [from,until]
+     def quickSort(arr: Array[SimpleTuple]): Array[SimpleTuple] = {
+       if( arr.length <= 1 ){
+         arr
+       }else{
+         val pivot = arr ( arr.length / 2 )
+         Array.concat (
+           quickSort ( arr filter (pivot.value < _.value) ),
+           arr filter (pivot.value == _.value),
+           quickSort ( arr filter (pivot.value > _.value) )
+         )
+       }
+     }
+
+    var ys = new Array[SimpleTuple]( until - from )
+    var i = from;
+    var j = 0;
+
+    while (i<until) {
+      ys(j) = xs(i)
+      i = i+1;
+      j = j+1;
+    }
+
+    ys = quickSort(ys)
+
+    i = from;
+    j = 0;
+    while (i<until) {
+      xs(i) = ys(j)
+      i = i+1;
+      j = j+1;
+    }
+
+   }
+
+
+/*
 
   def SegmentQuickSort(xs: Array[SimpleTuple], from: Int, until: Int) = {
 
@@ -137,9 +220,7 @@ object Sorting {
     }
 
    }
-
-
-
+* */
 
 
 
