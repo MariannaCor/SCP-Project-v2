20d20b1bce658530a57919eddb19d16364017267
Salvo <sfiorilla01@gmail.com>
2021-04-27 18:19:24 +0200

    bug fixes and improvement

--- src/main/scala/ServiceMain.scala
+++ src/main/scala/ServiceMain.scala
@@ -1,3 +1,5 @@
+import java.lang.ClassCastException
+
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.{coalesce, col, explode}
@@ -16,34 +18,54 @@ import scala.collection.mutable
 object ServiceMain {
 
   // LOCAL MARY
-/*
+  /*
   val path = ""
   val pathWikiClean = "wikiclean.json"
   val pathIdTitleTextDB = "idTitleTextDB.json"
   val pathWikiDB = "wikidb.json"
   val pathOutput = "outputTests/"
-*/
+  */
 
   // LOCAL SAL
-  val pathOfReading = "C:\\Users\\Salvo\\GitHub\\data\\jsonFiles\\"
-  val pathOfWriting = "outputFolder\\"
+ /* val whereReadingDB = "C:\\Users\\Salvo\\GitHub\\data\\jsonFiles\\"
+  val wherePutOutput = "outputFolder\\"
+  val pathOfWikiClean = wherePutOutput + "wikidb\\"+"wikiclean.json"
+  val pathOfIdTitleTextDB =  wherePutOutput+"idTitleTextDB\\"+"idTitleTextDB.json"
+ */
 
-  val pathWikiClean = pathOfWriting+"wikiclean.json"
-  val pathIdTitleTextDB = pathOfWriting+"idTitleTextDB.json"
+  //AWS
+  //la prima viene usata nel preprocess quando viene letto uno degli n file.
+  val whereReadingDB = "s3://sal1/"
 
-  val pathWikiDB = "wikidb.json"
-  val pathOutput = "outputTests/"
+  //la seconda viene usata ogni volta che si scrive sul bucket.: si scrive in 3 occasioni e tutti usano wherePutOutput come root.
+  //1. alla fine del preprocess per salvare le rdd preprocessate
+  //2. si rinominano i file dopo la loro creazione dando come indirizzo quello che poi genera path of WikiClean e IdTitleTextDB
+  //3. infine l'ultima scrittura genera file che però non vengono rinominati ed è l'output finale
+
+  val wherePutOutput = "s3://sal1/outputFolder/"
+
+  //queste ultime due sono i path che vengono usati per leggere i dati in caso in cui si evita il preprocess
+  val pathOfWikiClean = wherePutOutput + "wikidb/"+"wikiclean.json"
+  val pathOfIdTitleTextDB =  wherePutOutput+"idTitleTextDB/"+"idTitleTextDB.json"
 
 
-/*
- //AWS
+  /*
+  *  !!!!!!!!!!!!! considerazioni da dire a marianna !!!!!!!!1
+  * per renderre il prog scalabile dovremmo preprocessare tutti i file e poi passare come parametro il numero di file da leggere.
+  * Secondo me per dimostrare la prof che funziona tutto ci basta lavorare non su 80GB ma anche su 1GB.
+  * Per questo motivo direi di fare l'upload di tutti i file json, poi ho inserito anche la possibilità di dare il comando
+  *  --numFile=3 per esempio per poter leggere 3 file.
+  *
+  * */
+
+  /*
  val path = "s3://scpmarysal/"
  val pathWikiClean = path+"wikiclean.json"
  val pathIdTitleTextDB = path+"idTitleTextDB.json"
  val pathWikiDB = path+"preProcessedDB"
  val pathOutput = path+"eccoIl30/"
 */
- /*
+  /*
     ReadXMLfromS3(spark: SparkSession, xmlFile: String): DataFrame = {
 
      val revisionStruct = StructType(
@@ -83,7 +105,6 @@ object ServiceMain {
      }
   }
 
-
   val x = res.filterNot( x => x == (0,0.0,"","") );
    x
  }
@@ -91,14 +112,12 @@ object ServiceMain {
  def generateParDF(idTitleTextDB: DataFrame, source: Array[(Int,Double)], spark: SparkSession) = {
 
    /* TEMPO DI ESECUZIONE di questa FUNZIONE
-
      {"id":"ParDF","text":"Elapsed time: 259ms"}
      {"id":"ParDF","text":"Elapsed time: 0sec"}
     */
    val ids = source.map(el => el._1.toString)
    idTitleTextDB.filter(col("id").isin(ids: _*))
  }
-
 
  def block_of_code(tokenizedPreprocessedDB: RDD[(Int, Array[String])], idTitleTextDB: DataFrame, myQuery: Array[String], sparkSession: SparkSession): Unit = {
 
@@ -114,21 +133,15 @@ object ServiceMain {
 
    val n = 10
    lazy val firstN = scores.top(n)( PairOrdering ); //fa il contrario di take ordering e torna i primi 10 con score più alto direttamente dalle RDD.. consuma molto meno memoria sul driver.
-
    firstN map (println(_))
-
    lazy val result = generateParDF(idTitleTextDB, firstN, sparkSession)
-
    //lazy val result = generateOutput(idTitleTextDB, firstN, sparkSession).seq
    //sc.parallelize(result).coalesce(1).saveAsTextFile("outputTests/output");
-
-   result.coalesce(1).write
-     .mode("overwrite")
-     .format("json")
-     .save("outputTests/output")
+   result.coalesce(1).write.mode("overwrite").format("json").save(wherePutOutput+"/output")
 
  }
-  def readFullDBFromJson(path: String, spark: SparkSession): Array[DataFrame] = {
+
+ def readFullDBFromJson(path: String, spark: SparkSession): Array[DataFrame] = {
 
     val dfDB = spark.read
       .option("multiline", "true")
@@ -139,15 +152,8 @@ object ServiceMain {
     val arrDB = dfDB.selectExpr("mediawiki.page")
     val tmpDF = arrDB.withColumn("page", explode(col("page")))
 
-    //tmpDF.printSchema()
-    //tmpDF.show(true);
-
-
     arrDF(0) = tmpDF.selectExpr("page.id", "page.revision.text.content")
     arrDF(1) = tmpDF.selectExpr("page.id", "page.title", "page.revision.text.content")
-
-    //arrDF(1).printSchema()
-    //arrDF(1).show(true)
 
     arrDF
 
@@ -173,32 +179,25 @@ object ServiceMain {
  def readPreprocessedDBFromJson(path: String, spark: SparkSession): DataFrame = {
    val dfDB = spark.read
      .json(path)
-
    dfDB
  }
-
 
  /*
  * @args param options example
  * -preprocess false/true keywords
  *
  * */
-
-
-
  //def fromDFtoRDD[T](sc: SparkContext, tokenizedPreprocessedDB: DataFrame) : RDD[T] = { volevo farla in questo modo vedi dopo come fare}
-
  def fromDFtoRDD(sc: SparkContext, tokenizedPreprocessedDB: DataFrame) =
-   sc.parallelize(tokenizedPreprocessedDB.collect())
+   sc.parallelize(  tokenizedPreprocessedDB.collect() )
      .map( row => (
        row.getLong(0).toInt,
        row.get(1).asInstanceOf[mutable.WrappedArray[String]].toArray[String]
      ))
 
 
-
-
  def main(args: Array[String]): Unit = {
+
    val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
    val spark: SparkSession = SparkSession.builder.config(conf).getOrCreate()
    spark.sparkContext.setLogLevel("WARN")
@@ -211,6 +210,54 @@ object ServiceMain {
    println("args => ")
    args.foreach( x => print(" " +x))
 
+   def checkandgetArgue(commands: Array[String]) = {
+
+     //ci devono essere 3 comandi se non ci sono questi 3 la query non può continuare.
+     //il modello corretto di query sarebbe
+     // --preprocess=true/false --numFile=N --query=""
+     var prep: Boolean = null
+     val prepIndex = commands.indexOf("--preprocess=")
+
+     prepIndex match {
+       case -1 => throw new IllegalArgumentException("--preprocess command is missing")
+       case _=>  prep = commands(prepIndex+1) match {
+           case "true" => true
+           case "false" =>false
+           case _ =>  throw new IllegalArgumentException("--preprocess can have only boolean value")
+         }
+     }
+
+     val nfIndex = commands.indexOf("--numFile=")
+     var numfile: Int = null
+
+     nfIndex match {
+       case -1 => throw new IllegalArgumentException("--numFile command is missing")
+       case _ => numfile = commands(nfIndex+1) match {
+         case value => try value.toInt
+             catch{
+               case ClassCastException =>  throw new IllegalArgumentException("--numFile can have only integer value ")
+             }
+       }
+     }
+
+     val queryIndex = commands.indexOf("--query=")
+     var query : String = null
+     queryIndex match {
+       case -1 => throw new IllegalArgumentException("--query command is missing")
+       case _ => query = commands(queryIndex+1) match {
+         //case "the query value is a words inside the upper quotes"
+       //case _ =>  throw new IllegalArgumentException("--query can have only a sentence between upper quotes \" \" ")
+         
+       }
+     }
+
+
+
+     (prep,numfile,query)
+   }
+
+
+
    //SPLIT ARGS
    //if check is necessary to avoid null pointer exception
    val (command,query) = if(args.length > 2 ) args.splitAt(2) else new Array[String](2) -> new Array[String](0)
@@ -219,26 +266,29 @@ object ServiceMain {
    (command(0), command(1) )match {
      case ("--preprocess","false") => {
        // READ PREPROCESSED DB
-       tokenizedPreprocessedDB = readPreprocessedDBFromJson(pathWikiClean, spark)
-       idTitleTextDB = readPreprocessedDBFromJson(pathIdTitleTextDB, spark)
+       tokenizedPreprocessedDB = readPreprocessedDBFromJson(pathOfWikiClean, spark)
+       idTitleTextDB = readPreprocessedDBFromJson(pathOfIdTitleTextDB, spark)
+
      }
      case ("--preprocess","true")  => {
        // PREPROCESS DB and WRITE IT on S3
-       val fullDB = readFullDBFromJson(pathOfReading + "wikiSplitted_3.json", spark)
+       val fullDB = readFullDBFromJson(whereReadingDB + "wikiSplitted_3.json", spark)
        val toBePreprocessedDB = fullDB(0)
        idTitleTextDB = fullDB(1)
 
        //PREPROCESS FULL DB
        val preProcessData = new DataPreprocessing(toBePreprocessedDB)
        tokenizedPreprocessedDB = preProcessData.preProcessDF()
+
        //WRITE PREPROCESSED DBs TO A FILE
        //lo mette dentro la cartella idTitleTextDB al percorso
-       idTitleTextDB.write.mode("overwrite").format("json").save(pathOfWriting + "idTitleTextDB")
-       tokenizedPreprocessedDB.write.mode("overwrite").format("json").save(pathOfWriting + "wikidb")
+       idTitleTextDB.write.mode("overwrite").format("json").save(wherePutOutput + "idTitleTextDB")
+       tokenizedPreprocessedDB.write.mode("overwrite").format("json").save(wherePutOutput + "wikidb")
 
        //RENAME DB FILE
-       renameDBFile("wikiclean.json", "preProcessedDB/", "scpmarysal", "us-east-1")
-       renameDBFile("idTitleTextDB.json", "idTitleTextDB/", "scpmarysal", "us-east-1")
+       //renameDBFile("wikiclean.json",  "preprocessedDB/", "scpmarysal", "us-east-1") //? non torna preprocessedDB
+       renameDBFile("wikiclean.json", wherePutOutput + "wikidb/", "scpmarysal", "us-east-1") //? non torna il preprocessedDB preprocessedDB
+       renameDBFile("idTitleTextDB.json", wherePutOutput+"idTitleTextDB/", "scpmarysal", "us-east-1") //?
      }
 
      case _ => throw new IllegalArgumentException("--preprocess is missing or was not recognized with value true or false")
@@ -258,7 +308,7 @@ object ServiceMain {
 
    // END OFFICIAL VERSION
 
- // TEST VERSION
+  // TEST VERSION
    /*
     val testSenteces = Seq(
       (1, Array("hi", "heard", "spark", "think", "spark", "beautiful")),
@@ -276,10 +326,7 @@ object ServiceMain {
   */
  }
 
-
-
-
-  def time[R](block: => R, spark: SparkSession, name: String): R = {
+ def time[R](block: => R, spark: SparkSession, name: String): R = {
 
    val t0 = System.nanoTime()
    val result = block // call-by-name
@@ -292,15 +339,10 @@ object ServiceMain {
    println("elapsed time =>  \n"+elapsedTimeSentence.map( x => "\n "+x._2))
 
    val elapsedTimeSentenceDataFrame = spark.createDataFrame(elapsedTimeSentence).toDF("id", "text")
-
-   elapsedTimeSentenceDataFrame.coalesce(1).write
-     .mode("overwrite")
-     .format("json")
-     .save(pathOutput + "timeOutput")
+   elapsedTimeSentenceDataFrame.coalesce(1).write.mode("overwrite").format("json").save(wherePutOutput + "timeOutput")
 
    result
  }
-
 
  def renameDBFile(newfileName: String, folder: String, bucketName: String, clientRegion: String): Unit = {
 
@@ -369,12 +411,7 @@ object ServiceMain {
 */
  }
 
-
-
 }
-
-
-
 
 case class SimpleTuple(idOfTheDoc: Long, value: Double) {
  override def toString: String = this.idOfTheDoc + ",\t" + this.value
